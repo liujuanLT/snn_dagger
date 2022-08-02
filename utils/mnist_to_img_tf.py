f406931e36f3a17d70bc4f676db9565892f7f549
liujuanLT <juan.liu@lightelligence.ai>
2022-08-02 11:25:26 +0800

    fix utils/mnist_to_img.tf,py, now the script success to create images with pixel value [0, 255]

--- utils/mnist_to_img_tf.py
+++ utils/mnist_to_img_tf.py
@@ -15,5 +15,10 @@ for i in range(test_images.shape[0]):
     n_map[label] += 1
     filename = os.path.join(save_dir, str(label), str(n_map[label])+'.png')
     img = np.reshape(img, (28, 28, 1))
+    img = img * 255.0 # [0.0, 1.0] to [0, 255]
+    subdir = os.path.dirname(filename)
+    if not os.path.exists(subdir):
+        os.makedirs(subdir)
     ret = cv2.imwrite(filename, img, [int(cv2.IMWRITE_PNG_COMPRESSION), 0]) # no compression
+    print(ret)
 print('done')
